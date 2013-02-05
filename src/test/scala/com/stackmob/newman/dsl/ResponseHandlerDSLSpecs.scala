2264e393581e4cb7efbd4f296068f0ba50b6c568
Kelsey Innis <kelsey@stackmob.com>
2013-02-05 13:37:01 -0800

    add a spec to see how custom errors actually work

--- src/test/scala/com/stackmob/newman/dsl/ResponseHandlerDSLSpecs.scala
+++ src/test/scala/com/stackmob/newman/dsl/ResponseHandlerDSLSpecs.scala
@@ -30,6 +30,7 @@ class ResponseHandlerDSLSpecs extends Specification { def is =
   """                                                                                                                   ^
   "The DSL should"                                                                                                      ^
     "return a Failure if the IO throws"                                                                                 ! ThrowingIO().returnsFailure ^
+    "return non throwable error types if specified"                                                                     ! CustomErrors().returnsCorrectly ^
                                                                                                                         end
   trait Context
 
@@ -43,6 +44,23 @@ class ResponseHandlerDSLSpecs extends Specification { def is =
         case e => e must beEqualTo(ex)
       }
     }
+  }
+
+  case class CustomErrors() extends CustomErrorContext {
+    def returnsCorrectly: SpecsResult = {
+      val ex = new Exception("test exception")
+      val customError = new CustomErrorForSpecs("test exception")
+      val respIO: IO[Validation[CustomErrorForSpecs, Unit]] = io((throw ex): HttpResponse).handleCode[CustomErrorForSpecs, Unit](HttpResponseCode.Ok)(_ => ().success)
+      respIO.unsafePerformIO.either must beLeft.like {
+        case e => e must beEqualTo(customError)
+      }
+    }
+  }
+
+  trait CustomErrorContext extends Context {
+    case class CustomErrorForSpecs(msg: String)
+
+    implicit def exToCustomError(ex: Throwable): CustomErrorForSpecs = new CustomErrorForSpecs(ex.getMessage())
   }
 
 }
