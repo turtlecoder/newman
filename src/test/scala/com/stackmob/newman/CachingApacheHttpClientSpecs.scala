173c11c70f8d0a0bde129fd025b3cb828c7e2e45
Aaron Schlesinger <arschles@gmail.com>
2013-02-14 17:53:50 -0800

    adding blank spec for CachingHttpClient

---
+++ src/test/scala/com/stackmob/newman/CachingApacheHttpClientSpecs.scala
@@ -0,0 +1,16 @@
+package com.stackmob.newman
+
+import org.specs2.Specification
+
+/**
+ * Created by IntelliJ IDEA.
+ * 
+ * com.stackmob.newman
+ * 
+ * User: aaron
+ * Date: 2/14/13
+ * Time: 5:53 PM
+ */
+class CachingApacheHttpClientSpecs extends Specification {
+
+}
