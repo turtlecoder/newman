47507488fd01ef70d92fd11e841a177d1448f756
Aaron Schlesinger <arschles@gmail.com>
2013-02-21 17:10:14 -0800

    scalacheck-ifying InMemoryHttpResponseCacherSpecs

--- src/test/scala/com/stackmob/newman/test/caching/InMemoryHttpResponseCacherSpecs.scala
+++ src/test/scala/com/stackmob/newman/test/caching/InMemoryHttpResponseCacherSpecs.scala
@@ -17,35 +17,69 @@
 package com.stackmob.newman.test
 package caching
 
-import scalaz._
-import Scalaz._
-import org.specs2.Specification
-import org.specs2.execute.{Result => SpecsResult}
 import com.stackmob.newman._
 import com.stackmob.newman.caching._
-import com.stackmob.newman.response._
+import com.stackmob.newman.test.scalacheck._
+import org.specs2.{ScalaCheck, Specification}
 import java.net.URL
+import java.util.concurrent.TimeUnit
+import org.scalacheck._
+import Prop._
+import request._
 
-class InMemoryHttpResponseCacherSpecs extends Specification { def is =
-  "InMemoryHttpResponseCacherSpecs".title                                                                               ^
-  "The InMemoryHttpResponseCacher implements an HttpResponseCacher in memory, in a thread-safe manner"                  ^
-  "The cacher should"                                                                                                   ^
-    "correctly round trip an HttpRequest"                                                                               ! RoundTrip().succeeds ^
+class InMemoryHttpResponseCacherSpecs extends Specification with ScalaCheck { def is =
+  "InMemoryHttpResponseCacherSpecs".title                                                                               ^ end ^
+  "The InMemoryHttpResponseCacher implements an HttpResponseCacher in memory, in a thread-safe manner"                  ^ end ^
+  "The cacher should correctly round trip an HttpRequest"                                                               ! roundTripSucceeds ^ end ^
+  "The cacher should correctly expire items after their TTL"                                                            ! ttlSucceeds ^ end ^
                                                                                                                         end
-  trait Context extends BaseContext {
-    protected val client = new DummyHttpClient
-    protected val request = client.get(new URL("http://stackmob.com"), Headers.empty)
-    protected val responseFn = client.responseToReturn
-    protected val cache: HttpResponseCacher = new InMemoryHttpResponseCacher
+  private val client = new DummyHttpClient()
+
+  private val genHeader: Gen[Header] = for {
+    key <- genNonEmptyString
+    value <- genNonEmptyString
+  } yield {
+    key -> value
   }
 
-  case class RoundTrip() extends Context {
-    def succeeds: SpecsResult = {
-      (cache.get(request).unsafePerformIO must beEqualTo(Option.empty[HttpResponse])) and
-      (cache.exists(request).unsafePerformIO must beFalse) and
-      (cache.set(request, responseFn(), Time.now).unsafePerformIO must beEqualTo(())) and
-      (cache.get(request).unsafePerformIO must beEqualTo(responseFn().some)) and
-      (cache.exists(request).unsafePerformIO must beTrue)
+  private val genHeaders: Gen[Headers] = for {
+    headers <- Gen.listOf(genHeader)
+  } yield {
+    Headers(headers)
+  }
+
+  private val genRequest: Gen[HttpRequest] = for {
+    urlString <- genNonEmptyString
+    url <- Gen.value(new URL("http://%s.com".format(urlString)))
+    headers <- genHeaders
+  } yield {
+    client.get(url, headers)
+  }
+
+  private val genCache: Gen[HttpResponseCacher] = {
+    Gen.value(new InMemoryHttpResponseCacher)
+  }
+
+  private def getResponse(request: HttpRequest) = {
+    request.prepare.unsafePerformIO
+  }
+
+  private def roundTripSucceeds = forAll(genRequest, genCache) { (request, cache) =>
+    val response = getResponse(request)
+    val getRes1 = cache.get(request).unsafePerformIO must beNone
+    val existsRes1 = cache.exists(request).unsafePerformIO must beFalse
+    val setRes1 = cache.set(request, response, Time(60, TimeUnit.SECONDS)).unsafePerformIO must beEqualTo(())
+    val getRes2 = cache.get(request).unsafePerformIO must beSome.like {
+      case s => s must beEqualTo(response)
     }
+    val existsRes2 = cache.exists(request).unsafePerformIO must beTrue
+    getRes1 and existsRes1 and setRes1 and getRes2 and existsRes2
+  }
+
+  private def ttlSucceeds = forAll(genRequest, genCache) { (request, cache) =>
+    val response = getResponse(request)
+    cache.set(request, response, Time(0, TimeUnit.MILLISECONDS)).unsafePerformIO
+    Thread.sleep(100)
+    cache.get(request).unsafePerformIO must beNone
   }
 }
