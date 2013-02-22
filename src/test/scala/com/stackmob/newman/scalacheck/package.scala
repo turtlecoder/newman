2550ef09dc9188c1888b10416bebaa203d5ce37f
Aaron Schlesinger <arschles@gmail.com>
2013-02-21 16:47:37 -0800

    adding some scalacheck generators

---
+++ src/test/scala/com/stackmob/newman/scalacheck/package.scala
@@ -0,0 +1,42 @@
+package com.stackmob.newman
+
+import caching.Time
+import org.scalacheck.Gen
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * com.stackmob.newman.tests
+ *
+ * User: aaron
+ * Date: 2/21/13
+ * Time: 3:49 PM
+ */
+package object scalacheck {
+  val genNonEmptyString: Gen[String] = for {
+    firstChar <- Gen.listOf1(Gen.alphaChar)
+    remainingChars <- Gen.listOf(Gen.alphaChar)
+  } yield {
+    val charList = List(firstChar) ++ remainingChars
+    charList.mkString
+  }
+
+  private val timeUnits = Seq(TimeUnit.DAYS,
+    TimeUnit.HOURS,
+    TimeUnit.MICROSECONDS,
+    TimeUnit.MILLISECONDS,
+    TimeUnit.MINUTES,
+    TimeUnit.NANOSECONDS,
+    TimeUnit.SECONDS)
+
+  val genTimeUnit: Gen[TimeUnit] = Gen.oneOf(timeUnits)
+
+  val genPositiveTime: Gen[Time] = for {
+    magnitude <- Gen.posNum[Int]
+    timeUnit <- genTimeUnit
+  } yield {
+    Time(magnitude, timeUnit)
+  }
+
+}
