cd1433c6eca00a3a656d2dde6565aa4eac957dfb
Aaron Schlesinger <arschles@gmail.com>
2013-02-21 17:57:09 -0800

    creating more generators

--- src/test/scala/com/stackmob/newman/test/scalacheck/package.scala
+++ src/test/scala/com/stackmob/newman/test/scalacheck/package.scala
@@ -1,18 +1,25 @@
+/**
+ * Copyright 2013 StackMob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.stackmob.newman.test
 
-import com.stackmob.newman.caching.Time
+import com.stackmob.newman.caching._
 import org.scalacheck.Gen
 import java.util.concurrent.TimeUnit
 
-/**
- * Created by IntelliJ IDEA.
- *
- * com.stackmob.newman.tests
- *
- * User: aaron
- * Date: 2/21/13
- * Time: 3:49 PM
- */
 package object scalacheck {
   val genNonEmptyString: Gen[String] = for {
     firstChar <- Gen.listOf1(Gen.alphaChar)
@@ -32,11 +39,23 @@ package object scalacheck {
 
   val genTimeUnit: Gen[TimeUnit] = Gen.oneOf(timeUnits)
 
-  val genPositiveTime: Gen[Time] = for {
-    magnitude <- Gen.posNum[Int]
-    timeUnit <- genTimeUnit
+  val genPositiveMilliseconds: Gen[Milliseconds] = for {
+    magnitude <- Gen.posNum[Long]
   } yield {
-    Time(magnitude, timeUnit)
+    Milliseconds(magnitude)
+  }
+
+  val genHashCode: Gen[HashCode] = for {
+    str <- genNonEmptyString
+  } yield {
+    str.getBytes
+  }
+
+  val genCachedResponseDelay = for {
+    ttl <- genPositiveMilliseconds
+    hashCode <- genHashCode
+  } yield {
+    CachedResponseDelay(ttl, hashCode)
   }
 
 }
